36656b495f8ceb2d12341d2a8f30ac80a7fde862
Maxfield <848483650@qq.com>
2026-05-26 19:21:43 +0800

    marker function

---
+++ Contents/mods/MaxFindMyBody/42/media/lua/client/FindMyBody/FindMyBodyClient.lua
@@ -0,0 +1,40 @@
+local FindMyBodyClient = {
+}
+local px, py, pz = nil, nil, nil
+local marker = nil
+
+
+function OnKeyPressed(key)
+end
+
+---@param player IsoPlayer
+function OnPlayerDeath(player)
+    px = player:getX()
+    py = player:getY()
+    pz = player:getZ()
+
+    if ISWorldMap:isVisible() and marker == nil then
+        marker = FindMyBodyClient.addMarker(px, py)
+    end
+end
+
+function FindMyBodyClient.reset()
+    if marker ~= nil then FindMyBodyClient.removeMarker(marker) end
+    marker = nil
+    px, py, pz = nil, nil, nil
+end
+
+function FindMyBodyClient.addMarker(x, y)
+    local mapAPI = ISWorldMap.mapAPI
+    local markers = mapAPI:getMarkersAPI()
+    return markers:addGridSquareMarker(x, y, 0.1, 0, 0, 255, 0.5)
+end
+
+function FindMyBodyClient.removeMarker(marker)
+        local mapAPI = ISWorldMap.mapAPI;
+    local markers = mapAPI:getMarkersAPI();
+    markers:removeMarker(marker);
+end
+
+Events.OnKeyPressed.Add(OnKeyPressed)
+Events.OnPlayerDeath.Add(OnPlayerDeath)
